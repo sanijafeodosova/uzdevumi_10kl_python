6dd86a6c35b60e10d0fc7c465c7591a54ce887ae
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2022-03-23 08:54:48 +0200

    Update uzd_01_3.py

--- uzd_01_20201210/uzd_01_3.py
+++ uzd_01_20201210/uzd_01_3.py
@@ -1,7 +1,2 @@
-# Izveidot programmu, kura prasa lietotâjam sekunşu skaitu. Sekundes tiek pârveidotas par “x hours y minutes z seconds” tipa tekstu. Rezultâts tiek parâdîts konsolç.
+# Izveidot programmu, kura prasa lietotÃ¢jam sekunÃ¾u skaitu. Sekundes tiek pÃ¢rveidotas par â€œx hours y minutes z secondsâ€ tipa tekstu. RezultÃ¢ts tiek parÃ¢dÃ®ts konsolÃ§.
 
-sec = int(input("Seconds: "))
-h = sec//(60*60)
-min = (sec-(h*60*60))//60
-sec2 = sec-(h*60*60)-(min*60)
-print(h, "hours", min, "minutes", round(sec2), "sec.")
