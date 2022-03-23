aa10571687bebf50a1313840c3c7fc51c41b4ec7
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2022-03-23 08:57:30 +0200

    Update uzd_03_5.py

--- uzd_03_20210107/uzd_03_5.py
+++ uzd_03_20210107/uzd_03_5.py
@@ -1,15 +1,3 @@
 # Uzdevums 03_05:
 # Atrast pozitīva skaitļa ciparu summu. Piemērs: 14214 => 12
 
-strSk = input("Ievadiet pozitīvu skaitli: ")
-sum = 0
-try:
-    sk = int(strSk)
-    if sk > 0:
-        for i in strSk:
-            sum += int(i)
-        print("Skaitļa ciparu summa: ", sum)
-    else:
-        print("Nav ievadīts pozitīvs skaitlis!")
-except ValueError:
-    print("Nav ievadīts skaitlis!")
