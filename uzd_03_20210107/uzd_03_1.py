65d4883b6b0f8ef7e4ee6ff7a2142d13e2112c03
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2022-03-23 08:56:43 +0200

    Update uzd_03_1.py

--- uzd_03_20210107/uzd_03_1.py
+++ uzd_03_20210107/uzd_03_1.py
@@ -1,14 +1,2 @@
-# Uzdevums 03_01:
 # Apgriezt pozitīva skaitļa ciparu secību. Piemērs: 12345 => 54321
 
-strSk = input("Ievadiet pozitīvu skaitli: ")
-try:
-    sk = int(strSk)
-    if sk > 0:
-        print("Apgrieztais skaitlis: ", end=" ")
-        for i in range(len(strSk)-1, -1, -1):
-            print(strSk[i], end="")
-    else:
-        print("Nav ievadīts pozitīvs skaitlis!")
-except ValueError:
-    print("Nav ievadīts skaitlis!")
