c2cbc3bc0c2439ed239329050a2419452129da26
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2022-03-23 08:58:00 +0200

    Update uzd_03_6.py

--- uzd_03_20210107/uzd_03_6.py
+++ uzd_03_20210107/uzd_03_6.py
@@ -1,10 +1,2 @@
-# Uzdevums 03_06:
-# Izveidot reizināšanas tabulu lietotāja ievadītam skaitlim.
 
-strSk = input("Ievadiet skaitli: ")
-try:
-    sk = int(strSk)
-    for i in range(10):
-        print(f"{sk}*{i+1}={sk*(i+1)}")
-except ValueError:
-    print("Nav ievadīts skaitlis!")
+# Izveidot reizināšanas tabulu lietotāja ievadītam skaitlim.
