db79fded1f902e0dd54f9a95f74a42011b24bfdb
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2022-03-23 08:54:20 +0200

    Update uzd_01_2.py

--- uzd_01_20201210/uzd_01_2.py
+++ uzd_01_20201210/uzd_01_2.py
@@ -1,13 +1,6 @@
-# Izveidot programmu, kura prasa lietotâjam ievadît cilindra râdiusu un tâ augstumu, tiek aprçíinâts cilindra laukums un tilpums. Rezultâts tiek parâdîts konsolç.
-# tilpums = 3.14 * râdiuss * râdiuss * augstums
-# laukums = 2 * (3.14 * râdiuss * râdiuss) + augstums * (2 * 3.14 * râdiuss)
+# Izveidot programmu, kura prasa lietotÃ¢jam ievadÃ®t cilindra rÃ¢diusu un tÃ¢ augstumu, tiek aprÃ§Ã­inÃ¢ts cilindra laukums un tilpums. RezultÃ¢ts tiek parÃ¢dÃ®ts konsolÃ§.
+# tilpums = 3.14 * rÃ¢diuss * rÃ¢diuss * augstums
+# laukums = 2 * (3.14 * rÃ¢diuss * rÃ¢diuss) + augstums * (2 * 3.14 * rÃ¢diuss)
 
-h = int(input("Enter the height: "))
-r = int(input("Enter the radius: "))
 
-pi = 3.14
-vol = pi * r**2 * h
-area = 2 * pi * r**2 + h * 2 * pi *r
-
-print("Volume: ", vol)
 print("Area: ", area)
