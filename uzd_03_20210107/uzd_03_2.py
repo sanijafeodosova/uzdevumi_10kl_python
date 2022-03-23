4847d7150b5739905bc9b38b3580126d2d2ec38d
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2022-03-23 08:56:59 +0200

    Update uzd_03_2.py

--- uzd_03_20210107/uzd_03_2.py
+++ uzd_03_20210107/uzd_03_2.py
@@ -1,11 +1,3 @@
-# Uzdevums 03_02:
+
 # Atrast cik skaitļi norādītajā intervālā dalās ar lietotāja norādīto dalītāju.
 
-start = int(input("Start: "))
-end = int(input("End: "))
-div = int(input("Divider: "))
-cnt = 0
-for i in range(start, end+1):
-    if i%div == 0:
-        cnt += 1
-print(f"{cnt} numbers between {start} and {end} can be divided by {div}")
