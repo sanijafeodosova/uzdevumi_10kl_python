fa86eaa61939defb8d87ddb110e6074289db5b4b
sanijafeodosova <sanija.feodosova@grobinasskola.lv>
2022-04-05 22:51:04 +0300

    Pirmais uzd

--- uzd_01_20201210/uzd_01_3.py
+++ uzd_01_20201210/uzd_01_3.py
@@ -1,2 +1,11 @@
 # Izveidot programmu, kura prasa lietotâjam sekunþu skaitu. Sekundes tiek pârveidotas par “x hours y minutes z seconds” tipa tekstu. Rezultâts tiek parâdîts konsolç.
 
+laiks = float(input("Ievadiet sekunžu skaitu: "))
+dienas = laiks // (24 * 3600)
+laiks = laiks % (24 * 3600)
+stundas = laiks // 3600
+laiks %= 3600
+minutes = laiks // 60
+laiks%=60
+seconds = laiks
+print("%d dienas %d stundas %d minūtes %d sekundes" % (dienas, stundas, minutes, seconds))
