c7b5897e4b80b1c832012db555bf70c336346098
sanijafeodosova <sanija.feodosova@grobinasskola.lv>
2022-04-19 15:49:32 +0300

    03_01

--- uzd_03_20210107/uzd_03_1.py
+++ uzd_03_20210107/uzd_03_1.py
@@ -1,3 +1,11 @@
 # Apgriezt pozitīva skaitļa ciparu secību. Piemērs: 12345 => 54321
 
-skaitlis = input("Ievadiet skaitli: ")
+skaitlis = int(input("Ievadiet skaitli: "))
+ 
+sk = 0
+while(skaitlis>0):
+  rev = skaitlis % 10
+  sk = (sk * 10) + rev
+  skaitlis = skaitlis//10
+ 
+print("Apgrieztais skaitlis ir : {}".format(sk))
